6f84abb9acb078cb4a361f2f9e04613c236a63d9
mgmilton <matt.milton@gmail.com>
2018-01-16 14:48:32 -0700

    adds test

--- test/features/user_sees_a_hompage_test.rb
+++ test/features/user_sees_a_hompage_test.rb
@@ -4,8 +4,13 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
-    save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
+  end
+
+  def test_user_sees_error
+    visit '/xxxxz'
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
   end
 end
