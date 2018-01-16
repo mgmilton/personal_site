f288a4a0b18abec9ad0c1722a33ad14db22a0295
mgmilton <matt.milton@gmail.com>
2018-01-16 16:34:56 -0700

    lol

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -5,7 +5,7 @@ class PersonalSite
     case env["PATH_INFO"]
     when '/' then index
     when '/about' then about
-    when '/main.css' then css
+    when '/index.css' then css
     else
       error
     end
@@ -13,10 +13,6 @@ class PersonalSite
 
   def self.index
     render_view('index.html')
-  end
-
-  def self.about
-    render_view('about.html')
   end
 
   def self.error
@@ -29,5 +25,9 @@ class PersonalSite
 
   def self.render_view(page, code = '200')
     [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
+  end
+
+  def self.render_static(asset)
+    [200, {'Content-Type' => 'text/html'}, [File.read("./public/#{asset}")]]
   end
 end
